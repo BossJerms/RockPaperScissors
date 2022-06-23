c6a58a1242d3127cb9cc0be63013c153a32e7d5b
Dav Lopez <mahsnickers552@gmail.com>
2022-06-23 11:29:54 +0800

    Initialized HTML and JavaScript file with randomizer

---
+++ script.js
@@ -0,0 +1,24 @@
+function computerPlay(){
+    const pool = ["rock", "paper", "scissors"];
+    return pool[Math.floor(Math.random()*pool.length)];
+}
+
+/*function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "It\'s a tie!"
+    }else if (){
+
+    }
+}
+*/
+
+
+
+/*let playerPoint = 0;
+let computerPoint = 0;
+
+for (let i = 0; i < 5; i++){
+
+}
+
+console.log(computerPlay())*/
